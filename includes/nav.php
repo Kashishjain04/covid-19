536aa6e5f8deb46c53e11fa9298f9b6a60c0a627
Kashish1904 <abhishekjain7204@gmail.com>
2020-06-02 19:38:26 +0530

    nav

---
+++ includes/nav.php
@@ -0,0 +1,27 @@
+
+<?php
+/*if(isset($_POST['search'])){
+    $state = ucwords($_POST['param']);
+    header("Location: state.php?name=".$state);
+}*/
+?>
+
+<nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                    <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" method="POST" action="state.php?name=">
+                            <div class="input-group"><input class="bg-light form-control border-0 small" type="text" name="param" placeholder="Search for State">
+                                <div class="input-group-append"><button class="btn btn-primary py-0" name="search"><i class="fas fa-search"></i></button></div>
+                            </div>
+                        </form>
+                        <ul class="nav navbar-nav flex-nowrap ml-auto">                            
+                            <li class="nav-item dropdown no-arrow mx-1" role="presentation">
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><span class="badge badge-danger badge-counter">3+</span><i class="fas fa-bell fa-fw"></i></a>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-list dropdown-menu-right animated--grow-in"
+                                        role="menu">
+                                        <h6 class="dropdown-header">alerts center</h6>
+                                        <a class="text-center dropdown-item small text-gray-500" href="#">Show All Alerts</a></div>
+                                </div>
+                            </li>                                                        
+                        </ul>
+                    </div>
+                </nav>
