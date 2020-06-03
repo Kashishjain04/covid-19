0d343b70c4169c70c199d2526cca28339d9c680f
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2020-06-03 07:35:13 +0530

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,7 +7,7 @@
         <hr class="sidebar-divider my-0" />
         <ul class="nav navbar-nav text-light" id="accordionSidebar">
             <li class="nav-item" role="presentation"><a class="nav-link active" href="/"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-            <li class="nav-item" role="presentation"><a class="nav-link" href="table.html"><i class="fa fa-newspaper"></i><span>News</span></a></li>
+            <li class="nav-item" role="presentation"><a class="nav-link" href="#"><i class="fa fa-newspaper"></i><span>News</span></a></li>
             </ul>
             <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
         </div>
