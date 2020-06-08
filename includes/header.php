fcf8d9e09790ab55719bbb0ca54cd4717a06fef8
Kashish Jain <jainabhishek7204@gmail.com>
2020-06-08 11:01:55 +0530

    Icon

--- includes/header.php
+++ includes/header.php
@@ -27,6 +27,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
-    <link rel="icon" href="https://image.flaticon.com/icons/png/512/2913/2913584.png">
+    <link rel="icon" href="https://image.flaticon.com/icons/png/512/2785/2785819.png">
     <script data-ad-client="ca-pub-4358811063308681" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark align-items-start sidebar toggled sidebar-dark accordion bg-gradient-primary p-0">
         <div class="container-fluid d-flex flex-column p-0">
         <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-            <div class="sidebar-brand-icon rotate-n-15"><image width="33px" src="https://image.flaticon.com/icons/png/512/2781/2781395.png" /></div>
+            <div class="sidebar-brand-icon rotate-n-15"><image width="33px" src="https://image.flaticon.com/icons/png/512/2785/2785819.png" /></div>
             <div class="sidebar-brand-text mx-3"><span>Covid-19 India</span></div>
         </a>
         <hr class="sidebar-divider my-0" />
