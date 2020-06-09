d63232e440a302fe182de86ec43171e9db0f5317
Kashish Jain <jainabhishek7204@gmail.com>
2020-06-09 09:34:25 +0530

    SEO

--- includes/header.php
+++ includes/header.php
@@ -24,6 +24,8 @@
     <meta name="title" content="Covid-19 Pandemic India" />
     <meta name="keywords" content="cov19india.live covid-19 pandemic coronavirus chart tracking live data doubling rate india state wise state-wise confirmed per million" />
     <meta name="description" content="Track Live status of coronavirus in India with State-wise and District-wise data with chart comparison of last 30 days and analytics like Confirmed per million and Doubling rate is also provided." />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="2785819.png" />
     <title><?= $title ?></title>
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/search.css">
