e9652b29e100f7789d3eb954c146d127252b358a
Kashish Jain <jainabhishek7204@gmail.com>
2020-06-11 17:01:45 +0530

    All commit

--- articles.php
+++ articles.php
@@ -1,5 +1,103 @@
 <?php
-$title='News';
+$title = "Headlines";
+$icon = "https://image.flaticon.com/icons/svg/2680/2680927.svg";
 include('includes/header.php');
-include('includes/news.php');
+include('includes/nav.php');
 ?>
+<link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">    
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row align-items-stretch retro-layout-2">
+          <div class="col-md-4">
+            <a <?= $ndata1['articles'][0]['url']?> class="h-entry mb-30 v-height gradient" style="background-image: url('<?= $ndata1['articles'][0]['urlToImage']?>');">
+              
+              <div class="text">
+                <h2><?= $ndata1['articles'][0]['title']?></h2>
+                <span class="small text-gray-500"><?= date("d-m-Y H:i", strtotime($ndata1['articles'][0]['publishedAt']))?></span>
+              </div>
+            </a>
+            <a <?= $ndata1['articles'][1]['url']?> class="h-entry v-height gradient" style="background-image: url('<?= $ndata1['articles'][1]['urlToImage']?>');">
+              
+              <div class="text">
+                <h2><?= $ndata1['articles'][1]['title']?></h2>
+                <span class="small text-gray-500"><?= date("d-m-Y H:i", strtotime($ndata1['articles'][1]['publishedAt']))?></span>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a <?= $ndata1['articles'][2]['url']?> class="h-entry img-5 h-100 gradient" style="background-image: url('<?= $ndata1['articles'][2]['urlToImage']?>');">
+              
+            <div class="text">
+                <h2><?= $ndata1['articles'][2]['title']?></h2>
+                <span class="small text-gray-500"><?= date("d-m-Y H:i", strtotime($ndata1['articles'][2]['publishedAt']))?></span>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a <?= $ndata1['articles'][3]['url']?> class="h-entry mb-30 v-height gradient" style="background-image: url('<?= $ndata1['articles'][3]['urlToImage']?>');">
+              
+            <div class="text">
+                <h2><?= $ndata1['articles'][3]['title']?></h2>
+                <span class="small text-gray-500"><?= date("d-m-Y H:i", strtotime($ndata1['articles'][3]['publishedAt']))?></span>
+              </div>
+            </a>
+            <a href="<?= $ndata1['articles'][4]['url']?>"class="h-entry v-height gradient" style="background-image: url('<?= $ndata1['articles'][4]['urlToImage']?>');">
+              
+            <div class="text">
+                <h2><?= $ndata1['articles'][4]['title']?></h2>
+                <span class="small text-gray-500"><?= date("d-m-Y H:i", strtotime($ndata1['articles'][4]['publishedAt']))?></span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12">
+            <h2>Recent Posts</h2>
+          </div>
+        </div>
+        <div class="row">
+          <?php
+            foreach($ndata1['articles'] as $Key => $article){
+              if($Key<5){
+                continue;
+              }
+              if($article['content']){
+              $link = '<a href="article.php?id=<?= $Key?>">Read More</a>';
+              }
+          ?>
+          <div class="col-lg-4 mb-4">
+            <div class="entry2">
+              <a href="single.html"><img src="<?php if($article['urlToImage']){echo $article['urlToImage'];} else{echo "https://miro.medium.com/max/1200/1*cEaeMuTvINqIgyYQMSJWUA.jpeg";}?>" alt="Image" class="img-fluid rounded"></a>
+              <div class="excerpt">              
+              <h2><a href="single.html"><?= $article['title']?></a></h2>
+              <div class="post-meta align-items-center text-left clearfix">                                
+                <span><?= date("d-m-Y H:i", strtotime($article['publishedAt']))?></span>
+              </div>
+              
+                <p><?= $article['description']?></p>
+            <p><a href="<?= $article['url']?>">Read More</a></p>
+              </div>
+            </div>
+          </div>   
+            <?php }?>       
+        </div>        
+      </div>
+    </div>
+  </div>
+
+
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+  <?php include('includes/footer.php');?>
--- includes/news.php
+++ includes/news.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://newsapi.org/v2/top-headlines?q=COVID&apiKey=4af9a74626774997bde9703717af2639&country=in",
+  CURLOPT_URL => "https://newsapi.org/v2/top-headlines?q=covid&apiKey=4af9a74626774997bde9703717af2639&country=in",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -17,5 +17,23 @@ curl_setopt_array($curl, array(
 
 $response = curl_exec($curl);
 $ndata = json_decode($response, true);
+curl_close($curl);
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://newsapi.org/v2/top-headlines?q=&apiKey=0d42d55acd7f4923aac9a7d8d9c432fb&country=in",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+  ),
+));
+
+$response1 = curl_exec($curl);
+$ndata1 = json_decode($response1, true);
 curl_close($curl);
 ?>
