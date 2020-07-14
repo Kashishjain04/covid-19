c21f434a38dea312b30ec5daf3029023ddaaf9fa
Kashish Jain <jainabhishek7204@gmail.com>
2020-07-14 22:35:03 +0530

    Team designation

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,8 @@
             <!-- Team item -->
                 <div class="col-xl-3 col-sm-6 mb-5">
                     <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://media-exp1.licdn.com/dms/image/C4D03AQGmJfba6MM8tA/profile-displayphoto-shrink_100_100/0?e=1599696000&v=beta&t=6DrsT4P7TiggGjSwwCPJVWLOJr_e37Ttm3Fem4agJB0" alt="Kashish Jain" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                        <h5 class="mb-0">Kashish Jain</h5><span class="small text-uppercase text-muted">Founder - Designer</span>
+                        <h5 class="mb-0">Kashish Jain</h5>
+                        <!-- <span class="small text-uppercase text-muted">Founder - Designer</span> -->
                         <ul class="social mb-0 list-inline mt-3">
                             <li class="list-inline-item"><a href="https://github.com/Kashishjain04" class="social-link"><i class="fa fa-github"></i></a></li>
                             <li class="list-inline-item"><a href="https://www.linkedin.com/in/kashish-jain-053ab9195/" class="social-link"><i class="fa fa-linkedin"></i></a></li>
@@ -27,7 +28,8 @@
             <!-- Team item -->
             <div class="col-xl-3 col-sm-6 mb-5">
                     <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://media-exp1.licdn.com/dms/image/C4E03AQH84I_Vv17XVA/profile-displayphoto-shrink_200_200/0?e=1599696000&v=beta&t=xy3oCYOSJ764j_GwqMW_jxoprSI--5Kfn5TMSwiHYJk" alt="Nitika Jain" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                        <h5 class="mb-0">Nitika Jain</h5><span class="small text-uppercase text-muted">Promotions Manager</span>
+                        <h5 class="mb-0">Nitika Jain</h5>
+                        <!-- <span class="small text-uppercase text-muted">Promotions Manager</span> -->
                         <ul class="social mb-0 list-inline mt-3">                            
                             <li class="list-inline-item"><a href="https://www.linkedin.com/in/nitika-jain-100a47147/" class="social-link"><i class="fa fa-linkedin"></i></a></li>
                             <li class="list-inline-item"><a href="https://twitter.com/Nitikajain1996" class="social-link"><i class="fa fa-twitter"></i></a></li>
